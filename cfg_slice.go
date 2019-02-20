185e0a48a31b396db1d7b499860597abd8a3c5e7
Joshua Medley <jmedleyx7@gmail.com>
2019-02-20 13:42:55 +0000

    extra update for goDoc

--- cfg_slice.go
+++ cfg_slice.go
@@ -5,7 +5,7 @@ import (
 	"github.com/jsonrouter/core/http"
 )
 
-// Returns a validation object that checks for a slice of integers
+// Slice returns a validation object that checks for a slice of integers
 func Slice(vc *Config) *Config {
 
 	return NewConfig(
